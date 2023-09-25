08e54f62a3d320bdc89eaf522394f89d4d07f9ae
klym4uk <klym4uk.ant0n@gmail.com>
2023-09-25 11:36:53 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lab_1_1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lab_1_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn= findViewById(R.id.change_color);
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(MainActivity.this, "wow, color changed", Toast.LENGTH_SHORT).show();
+           TextView change_text= findViewById(R.id.textView);
+           change_text.setText("Hello people");
+
+           }
+       });
+    }
+}
