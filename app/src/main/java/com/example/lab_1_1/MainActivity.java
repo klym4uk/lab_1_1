df1ec7a9a695bda90b276f2b13721726d13dd476
klym4uk <klym4uk.ant0n@gmail.com>
2023-09-25 21:53:02 +0300

    final version 2.0, chatGPT helped to generate random colors and text)))

--- app/src/main/java/com/example/lab_1_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1_1/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,27 +18,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn= findViewById(R.id.change_text);
-       btn.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Toast.makeText(MainActivity.this, "wow, TEXT changed", Toast.LENGTH_SHORT).show();
-           TextView change_text= findViewById(R.id.textView);
-           change_text.setText("Hello people!");
-           // find text
+        Button btn = findViewById(R.id.change_text);
+        TextView change_text = findViewById(R.id.textView);
 
-           }
-       });
+// An array of possible text options
+        String[] randomTextOptions = {
+                "Hello PEOPLE!",
+                "Hello PROFE$$OR!",
+                "Random Text!",
+                "Have a nice day!"
+        };
 
-       Button btn2= findViewById(R.id.change_color);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "wow, TEXT changed", Toast.LENGTH_SHORT).show();
+
+                // A random index to select a random text from the array
+                int randomIndex = new Random().nextInt(randomTextOptions.length);
+
+                // Set the text to the randomly selected option
+                change_text.setText(randomTextOptions[randomIndex]);
+            }
+        });
+
+        Button btn2= findViewById(R.id.change_color);
        btn2.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
                Toast.makeText(MainActivity.this, "wow, COLOR changed", Toast.LENGTH_SHORT).show();
-            TextView change_text_color=findViewById(R.id.textView);
-            change_text_color.setTextColor(Color.RED);
+               TextView change_text_color = findViewById(R.id.textView);
 
+               // Generate a random color
+               int randomColor = generateRandomColor();
+
+               // Set the text color to the random color
+               change_text_color.setTextColor(randomColor);
            }
+
+           private int generateRandomColor() {
+               Random rand = new Random();
+               // Generate random values for the red, green, and blue components
+               int red = rand.nextInt(256);
+               int green = rand.nextInt(256);
+               int blue = rand.nextInt(256);
+               // Create the color by combining the components
+               return Color.rgb(red, green, blue);
+           }
+
        });
     }
 }
