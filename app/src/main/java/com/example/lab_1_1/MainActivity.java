34bb231ddfec7025751f4e3b997156e8f0f99efb
klym4uk <klym4uk.ant0n@gmail.com>
2023-09-25 12:01:44 +0300

    final version

--- app/src/main/java/com/example/lab_1_1/MainActivity.java
+++ app/src/main/java/com/example/lab_1_1/MainActivity.java
@@ -2,13 +2,12 @@ package com.example.lab_1_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +16,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn= findViewById(R.id.change_color);
+        Button btn= findViewById(R.id.change_text);
        btn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View view) {
-               Toast.makeText(MainActivity.this, "wow, color changed", Toast.LENGTH_SHORT).show();
+               Toast.makeText(MainActivity.this, "wow, TEXT changed", Toast.LENGTH_SHORT).show();
            TextView change_text= findViewById(R.id.textView);
-           change_text.setText("Hello people");
+           change_text.setText("Hello people!");
            // find text
+
+           }
+       });
+
+       Button btn2= findViewById(R.id.change_color);
+       btn2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(MainActivity.this, "wow, COLOR changed", Toast.LENGTH_SHORT).show();
+            TextView change_text_color=findViewById(R.id.textView);
+            change_text_color.setTextColor(Color.RED);
 
            }
        });
